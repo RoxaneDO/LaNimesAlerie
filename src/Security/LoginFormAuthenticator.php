2cca1939ea178ecb2a98254d0446df9e1d2d57ad
RoxaneDO <roxane.donnat@gmail.com>
2021-08-27 17:07:12 +0200

    relié le dashboard et le projet

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -50,9 +50,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('cgv'));
+
     }
 
     protected function getLoginUrl(Request $request): string
